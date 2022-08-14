9a24f223b85d4b5c13114437b380efbeed81e712
MariaGeorge55 <george.maria48@yahoo.com>
2022-08-14 18:15:57 +0200

    update function

--- syndicai.py
+++ syndicai.py
@@ -15,14 +15,14 @@ class PythonPredictor:
 
     def __init__(self, config):
         """ Download pretrained model. """
-        #self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).autoshape()
-        self.model = torch.jit.load('model_scripted_defect.pt').autoshape()
+        self.model = torch.hub.load('MariaGeorge55/Defect-inspection', 'model_scripted_defect.pt', pretrained=True)#.autoshape()
+        #self.model = torch.jit.load('model_scripted_defect.pt').autoshape()
 
-    def predict(self, payload):
+    def predict(self, img):
         """ Run a model based on url input. """
 
         # Inference
-        img = url_to_img(payload["url"])
+        #img = url_to_img(payload["url"])
         outputs = self.model(img)
         _, preds = torch.max(outputs, 1)
         pred = preds.cpu()
